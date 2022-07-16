526eb19530983a3dbfccd345855418331cfc43a5
Pedro Nery <pedrohnery@gmail.com>
2022-07-16 13:30:00 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,13 @@ import Header from "./components/Header";
 import NotificationButon from "./components/NotificationButton";
 import SalesCard from "./components/SalesCard";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
