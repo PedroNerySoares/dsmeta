2fbacbc40a79efec10bbbb0082dd2e6cac089b2e
Pedro Nery <pedrohnery@gmail.com>
2022-07-16 11:02:59 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -20,10 +23,14 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Sale>> findSales(){
+//	public ResponseEntity<List<Sale>> findSales(){
+	public ResponseEntity<Page<Sale>> findSales(
+												@RequestParam(value="minDate",defaultValue = "") String minDate,
+												@RequestParam(value="maxDate",defaultValue = "") String maxDate,
+												Pageable pageable){
 		
 		
-		return ResponseEntity.ok().body(service.findSales());
+		return ResponseEntity.ok().body(service.findSales(minDate,maxDate,pageable));
 //		return (List<Sale>) ResponseEntity.ok().body(service.findSales());
 	}
 }
