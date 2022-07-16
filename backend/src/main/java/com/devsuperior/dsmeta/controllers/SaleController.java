23ec3feaad6fa78c5da05ff6558aab22e3b7da49
Pedro Nery <pedrohnery@gmail.com>
2022-07-16 13:28:16 -0300

    Ajuste

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -6,6 +6,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,7 +39,9 @@ public class SaleController {
 //		return (List<Sale>) ResponseEntity.ok().body(service.findSales());
 	}
 	@GetMapping("/{id}/notification")
-	public void notifySms(@PathVariable Long id) {
+	public ResponseEntity<Sale> notify(@PathVariable Long id) {
 		smsService.sendSms(id);
+		return ResponseEntity.status(200).body(null);
 	}
+	
 }
